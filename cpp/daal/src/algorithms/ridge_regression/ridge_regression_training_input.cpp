6f31dc72d50deb22b5ebd12f9ae40353c2d0d6b6
Khalil Asadzade <khalil.asadzade@intel.com>
2025-02-19 07:56:42 -0800

    fix: returned to 'default' as definition for assign op

--- cpp/daal/src/algorithms/ridge_regression/ridge_regression_training_input.cpp
+++ cpp/daal/src/algorithms/ridge_regression/ridge_regression_training_input.cpp
@@ -38,11 +38,7 @@ namespace interface1
 {
 Input::Input() : linear_model::training::Input(lastInputId + 1) {}
 Input::Input(const Input & other) : linear_model::training::Input(other) {}
-Input & Input::operator=(const Input & other)
-{
-    linear_model::training::Input::operator=(other);
-    return *this;
-}
+Input & Input::operator=(const Input & other) = default;
 
 /**
  * Returns an input object for ridge regression model-based training
