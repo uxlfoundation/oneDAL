93d11f6ea1ed527df1f3edd4f30f4fe3312b3e0f
Ian Faust <icfaust@gmail.com>
2025-02-21 19:03:19 +0100

    Update adaboost_train_dense_default_batch_fpt_dispatcher.cpp

--- cpp/daal/src/algorithms/adaboost/adaboost_train_dense_default_batch_fpt_dispatcher.cpp
+++ cpp/daal/src/algorithms/adaboost/adaboost_train_dense_default_batch_fpt_dispatcher.cpp
@@ -52,12 +52,9 @@ Batch<algorithmFPType, method>::Batch(const Batch & other) : classifier::trainin
     initialize();
 }
 
-#define INSTANTIATE_CONSTRUCTORS(algorithmFPType, method)           \
-    template Batch<algorithmFPType, method>::Batch(size_t);         \
-    template Batch<algorithmFPType, method>::Batch(const Batch &);  \
-
-INSTANTIATE_CONSTRUCTORS(DAAL_FPTYPE, adaboost::training::defaultDense);
-INSTANTIATE_CONSTRUCTORS(DAAL_FPTYPE, adaboost::training::sammeR);
+#define INSTANTIATE_CONSTRUCTORS(algorithmFPType, method)   \
+    template Batch<algorithmFPType, method>::Batch(size_t); \
+    template Batch<algorithmFPType, method>::Batch(const Batch &)
 
 } // namespace interface2
 } // namespace training
