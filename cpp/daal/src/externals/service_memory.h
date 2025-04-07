6d0d6137db272501bd373c3e99dc0db5b7bab3f5
Victoriya Fedotova <victoriya.s.fedotova@intel.com>
2025-04-07 01:39:31 -0700

    Use memory aligned loads and stores where possible in DAAL memory management

--- cpp/daal/src/externals/service_memory.h
+++ cpp/daal/src/externals/service_memory.h
@@ -29,34 +29,133 @@
 #include "src/services/service_defines.h"
 #include "src/threading/threading.h"
 
+#include <climits>     // UINT_MAX
+#include <type_traits> // is_trivially_default_constructible
+
 namespace daal
 {
 namespace services
 {
 namespace internal
 {
+
+/// Initialize block of memory of length num with value.
+///
+/// @tparam T   Data type of the elements in the block.
+/// @tparam cpu Variant of the CPU instruction set: SSE2, SSE4.2, AVX2, AVX512, ARM SVE, etc.
+///
+/// @param ptr      Pointer to the block of memory.
+/// @param value    Value to initialize the block with.
+/// @param num      Number of elements in the block.
 template <typename T, CpuType cpu>
-T * service_calloc(size_t size, size_t alignment = 64)
+void service_memset_seq(T * const ptr, const T value, const size_t num)
 {
-    T * ptr = (T *)daal::services::daal_malloc(size * sizeof(T), alignment);
+    if (num < UINT_MAX && !((DAAL_UINT64)ptr & DAAL_DEFAULT_ALIGNMENT_MASK))
+    {
+        /// Use aligned stores
+        const unsigned int num32 = static_cast<unsigned int>(num);
+        PRAGMA_IVDEP
+        PRAGMA_VECTOR_ALWAYS
+        PRAGMA_VECTOR_ALIGNED
+        for (unsigned int i = 0; i < num32; i++)
+        {
+            ptr[i] = value;
+        }
+    }
+    else
+    {
+        PRAGMA_IVDEP
+        PRAGMA_VECTOR_ALWAYS
+        for (size_t i = 0; i < num; i++)
+        {
+            ptr[i] = value;
+        }
+    }
+}
+
+template <CpuType cpu>
+struct ServiceInitializer
+{
+    template <typename T>
+    static void memset_default(T * const ptr, const size_t num)
+    {
+        service_memset_seq<T, cpu>(ptr, T(), num);
+    }
+
+    template <typename T>
+    static void memset(T * const ptr, const size_t num)
+    {
+        char * cptr = (char *)ptr;
+        size_t size = num * sizeof(T);
+
+        if (size < UINT_MAX && !((DAAL_UINT64)cptr & DAAL_DEFAULT_ALIGNMENT_MASK))
+        {
+            /// Use aligned stores
+            unsigned int size32 = static_cast<unsigned int>(size);
+            PRAGMA_IVDEP
+            PRAGMA_VECTOR_ALWAYS
+            PRAGMA_VECTOR_ALIGNED
+            for (unsigned int i = 0; i < size32; i++)
+            {
+                cptr[i] = '\0';
+            }
+        }
+        else
+        {
+            PRAGMA_IVDEP
+            PRAGMA_VECTOR_ALWAYS
+            for (size_t i = 0; i < size; i++)
+            {
+                cptr[i] = '\0';
+            }
+        }
+    }
+};
+
+template <typename T, CpuType cpu>
+T * service_calloc(size_t size, size_t alignment = DAAL_MALLOC_DEFAULT_ALIGNMENT)
+{
+    T * ptr = (T *)daal::services::daal_malloc(size, alignment);
     if (ptr == NULL)
     {
         return NULL;
     }
 
-    char * const cptr        = (char *)ptr;
-    const size_t sizeInBytes = size * sizeof(T);
-
-    for (size_t i = 0; i < sizeInBytes; i++)
+    if constexpr (std::is_trivially_default_constructible_v<T>)
     {
-        cptr[i] = '\0';
+        ServiceInitializer<cpu>::template memset_default<T>(ptr, size);
+    }
+    else
+    {
+        ServiceInitializer<cpu>::template memset<T>(ptr, size);
     }
 
     return ptr;
 }
 
 template <typename T, CpuType cpu>
-T * service_malloc(size_t size, size_t alignment = 64)
+T * service_scalable_calloc(size_t size, size_t alignment = DAAL_MALLOC_DEFAULT_ALIGNMENT)
+{
+    T * ptr = (T *)threaded_scalable_malloc(size * sizeof(T), alignment);
+    if (!ptr)
+    {
+        return nullptr;
+    }
+
+    if constexpr (std::is_trivially_default_constructible_v<T>)
+    {
+        ServiceInitializer<cpu>::template memset_default<T>(ptr, size);
+    }
+    else
+    {
+        ServiceInitializer<cpu>::template memset<T>(ptr, size);
+    }
+
+    return ptr;
+}
+
+template <typename T, CpuType cpu>
+T * service_malloc(size_t size, size_t alignment = DAAL_MALLOC_DEFAULT_ALIGNMENT)
 {
     return (T *)daal::services::daal_malloc(size * sizeof(T), alignment);
 }
@@ -68,28 +167,7 @@ void service_free(T * ptr)
 }
 
 template <typename T, CpuType cpu>
-T * service_scalable_calloc(size_t size, size_t alignment = 64)
-{
-    T * ptr = (T *)threaded_scalable_malloc(size * sizeof(T), alignment);
-
-    if (ptr == NULL)
-    {
-        return NULL;
-    }
-
-    char * const cptr        = (char *)ptr;
-    const size_t sizeInBytes = size * sizeof(T);
-
-    for (size_t i = 0; i < sizeInBytes; i++)
-    {
-        cptr[i] = '\0';
-    }
-
-    return ptr;
-}
-
-template <typename T, CpuType cpu>
-T * service_scalable_malloc(size_t size, size_t alignment = 64)
+T * service_scalable_malloc(size_t size, size_t alignment = DAAL_MALLOC_DEFAULT_ALIGNMENT)
 {
     return (T *)threaded_scalable_malloc(size * sizeof(T), alignment);
 }
@@ -125,18 +203,6 @@ T * service_memset(T * const ptr, const T value, const size_t num)
         }
     });
     return ptr;
-}
-
-/* Initialize block of memory of length num value */
-template <typename T, CpuType cpu>
-void service_memset_seq(T * const ptr, const T value, const size_t num)
-{
-    PRAGMA_IVDEP
-    PRAGMA_VECTOR_ALWAYS
-    for (size_t i = 0; i < num; i++)
-    {
-        ptr[i] = value;
-    }
 }
 
 /* Initialize block of memory of length num with entries [startValue, ..., startValue + num -1]*/
