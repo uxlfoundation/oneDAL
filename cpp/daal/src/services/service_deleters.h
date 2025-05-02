62030c41cd9ba70adf2e5ff78ef26c086df7ef34
Victoriya Fedotova <victoriya.s.fedotova@intel.com>
2025-05-02 02:58:31 -0700

    Copyright fix

--- cpp/daal/src/services/service_deleters.h
+++ cpp/daal/src/services/service_deleters.h
@@ -1,6 +1,6 @@
 /* file: service_deleters.h */
 /*******************************************************************************
-* Copyright 2015 Intel Corporation
+* Copyright contributors to the oneDAL project
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
