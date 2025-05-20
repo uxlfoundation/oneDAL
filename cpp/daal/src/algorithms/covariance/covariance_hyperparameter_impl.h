e354b85023774f9f16148b35d62844542ddf1742
Victoriya Fedotova <victoriya.s.fedotova@intel.com>
2025-05-20 05:17:23 -0700

    Fix hyperparameterIdCount in Covariance

--- cpp/daal/src/algorithms/covariance/covariance_hyperparameter_impl.h
+++ cpp/daal/src/algorithms/covariance/covariance_hyperparameter_impl.h
@@ -43,7 +43,7 @@ enum HyperparameterId
 {
     denseUpdateStepBlockSize = 0,
     denseUpdateStepGrainSize = 1,
-    hyperparameterIdCount    = denseUpdateStepGrainSize + 1
+    hyperparameterIdCount    = 2
 };
 
 enum DoubleHyperparameterId
