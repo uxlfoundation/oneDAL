6e9e8e2a3d3e50606301ca68766fb193f37559d0
richard.north.iii <rnorthii@smtp.intel.com>
2025-06-18 16:56:33 -0700

    Update examples
    Signed-off-by: richard.north.iii <rnorthii@smtp.intel.com>

--- examples/oneapi/cpp/source/cosine_distance/cosine_distance_dense_batch.cpp
+++ examples/oneapi/cpp/source/cosine_distance/cosine_distance_dense_batch.cpp
@@ -22,10 +22,11 @@
 namespace dal = oneapi::dal;
 
 int main(int argc, char const *argv[]) {
-    const auto data_file_name = get_data_path("distance.csv");
+    const auto x_data_file_name = get_data_path("x_distance.csv");
+    const auto y_data_file_name = get_data_path("y_distance.csv");
 
-    const auto x = dal::read<dal::table>(dal::csv::data_source{ data_file_name });
-    const auto y = dal::read<dal::table>(dal::csv::data_source{ data_file_name });
+    const auto x = dal::read<dal::table>(dal::csv::data_source{ x_data_file_name });
+    const auto y = dal::read<dal::table>(dal::csv::data_source{ y_data_file_name });
     const auto distance_desc = dal::cosine_distance::descriptor{};
 
     const auto result = dal::compute(distance_desc, x, y);
