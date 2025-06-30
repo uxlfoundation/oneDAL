e938b8d6cd88db7390b501ac30dfa4f8a47a9829
richard.north.iii <rnorthii@smtp.intel.com>
2025-06-30 08:39:09 -0700

    Update backend/cpu/compute_kernel_dense.cpp
    Signed-off-by: richard.north.iii <rnorthii@smtp.intel.com>

--- cpp/oneapi/dal/algo/correlation_distance/backend/cpu/compute_kernel_dense.cpp
+++ cpp/oneapi/dal/algo/correlation_distance/backend/cpu/compute_kernel_dense.cpp
@@ -83,11 +83,13 @@ struct compute_kernel_cpu<Float, method::dense, task::compute> {
                         const input_t& input) const {
         return compute<Float>(ctx, desc, input);
     }
+
 #ifdef ONEDAL_DATA_PARALLEL
     void operator()(const context_cpu& ctx,
                     const descriptor_t& desc,
-                    const table& data,
-                    bool& res) const {
+                    const table& x,
+                    const table& y,
+                    homogen_table& res) const {
         throw unimplemented(dal::detail::error_messages::method_not_implemented());
     }
 #endif
