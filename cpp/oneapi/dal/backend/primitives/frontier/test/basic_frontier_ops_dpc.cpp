7c774a69170293e8a417b92a053191033975c473
antonio-decaro <antonio.decaro99@outlook.it>
2025-07-08 14:04:53 +0000

    insert compunte active kernel

--- cpp/oneapi/dal/backend/primitives/frontier/test/basic_frontier_ops_dpc.cpp
+++ cpp/oneapi/dal/backend/primitives/frontier/test/basic_frontier_ops_dpc.cpp
@@ -23,9 +23,9 @@ TEST("frontier basic operations", "[frontier]") {
         bitset<std::uint32_t>::element_bitsize;
 
     SECTION("frontier has correct data layer size", "[frontier]") {
-        REQUIRE(f.get_data_layer_size() == expected_data_layer_size);
+        REQUIRE(f.get_data_size() == expected_data_layer_size);
 
-        REQUIRE(f.get_mlb_layer_size() == expected_mlb_layer_size);
+        REQUIRE(f.get_mlb_size() == expected_mlb_layer_size);
     }
 
     SECTION("frontier can insert elements", "[frontier]") {
@@ -33,8 +33,8 @@ TEST("frontier basic operations", "[frontier]") {
         view.insert(2);
         view.insert(bitset<std::uint32_t>::element_bitsize);
 
-        auto data_layer = f.get_data_layer();
-        auto mlb_layer = f.get_mlb_layer();
+        auto data_layer = f.get_data_ptr();
+        auto mlb_layer = f.get_mlb_ptr();
 
         REQUIRE(data_layer[0] == static_cast<uint32_t>(5));
         REQUIRE(data_layer[1] == static_cast<uint32_t>(1));
@@ -79,11 +79,8 @@ TEST("frontier queue basic operations", "[frontier]") {
         f.insert(2);
         f.insert(bitset<std::uint32_t>::element_bitsize);
 
-        auto data_layer = f.get_data_layer();
-        auto mlb_layer = f.get_mlb_layer();
-
-        std::cout << "Data layer size: " << data_layer[0] << std::endl;
-        std::cout << "MLB layer size: " << mlb_layer[0] << std::endl;
+        auto data_layer = f.get_data_ptr();
+        auto mlb_layer = f.get_mlb_ptr();
 
         REQUIRE(data_layer[0] == static_cast<uint32_t>(5));
         REQUIRE(data_layer[1] == static_cast<uint32_t>(1));
@@ -106,5 +103,26 @@ TEST("frontier queue basic operations", "[frontier]") {
     }
 
 } // TEST "frontier queue operations"
+
+TEST("compute active frontier", "[frontier]") {
+    sycl::queue queue{ sycl::default_selector_v };
+
+    const std::size_t num_items = 100000;
+    auto f = frontier<std::uint32_t>(queue, num_items);
+    auto view = f.get_device_view();
+
+    view.insert(0);
+    view.insert(2);
+    view.insert(32);
+    view.insert(1024);
+
+    auto e = f.compute_active_frontier();
+    e.wait();
+
+    // auto offsets = f.get_offsets_ptr();
+    auto offsets_size = f.get_offsets_size_ptr();
+
+    REQUIRE(offsets_size[0] == 2);
+} // TEST "compute active frontier"
 
 } // namespace oneapi::dal::backend::primitives::test
