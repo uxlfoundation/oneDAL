139e93e23cd989d3fe83f905f4bab42efdba3bf2
shubham.chaudhari <Shubham.Chaudhari@fujitsu.com>
2025-07-24 13:21:51 +0530

    Replace general FP32 scalar exp with SLEEF version in service_math_ref.h

--- cpp/daal/src/algorithms/kernel_function/kernel_function_rbf_helper.h
+++ cpp/daal/src/algorithms/kernel_function/kernel_function_rbf_helper.h
@@ -22,11 +22,6 @@
 #if defined(TARGET_ARM)
     #if (__CPUID__(DAAL_CPU) == __sve__)
         #include <arm_sve.h>
-        #define R_LN2f          1.4426950408889634f
-        #define L2Uf            0.693145751953125f
-        #define L2Lf            1.428606765330187045e-06f
-        #define SLEEF_INFINITYf __builtin_inff()
-
     #endif
 #endif
 namespace daal
@@ -164,50 +159,6 @@ inline services::Status HelperKernelRBF<double, sve>::postGemmPart(double * cons
 
     return services::Status();
 }
-//Sleef implementation of float exp function for SVE taken from https://github.com/shibatch/sleef/blob/ae6ad6cdfc1cb837685a8f40bb1447afce49ded2/src/libm/sleefsimdsp.c#L1313
-inline svfloat32_t exp_sleef(svfloat32_t d)
-{
-    svbool_t pg = svptrue_b32();
-
-    // q = (int)(d * R_LN2f)
-    svfloat32_t qf = svmul_f32_x(pg, d, svdup_f32(R_LN2f));
-    svint32_t q    = svcvt_s32_f32_x(pg, svrintn_f32_x(pg, qf));
-
-    // s = d - q * L2Uf - q * L2Lf
-    svfloat32_t s = svmad_f32_x(pg, svcvt_f32_s32_x(pg, q), svdup_f32(-L2Uf), d);
-    s             = svmad_f32_x(pg, svcvt_f32_s32_x(pg, q), svdup_f32(-L2Lf), s);
-
-    // Polynomial evaluation
-    svfloat32_t u = svdup_f32(0.000198527617612853646278381f);
-    u             = svmad_f32_x(pg, u, s, svdup_f32(0.00139304355252534151077271f));
-    u             = svmad_f32_x(pg, u, s, svdup_f32(0.00833336077630519866943359f));
-    u             = svmad_f32_x(pg, u, s, svdup_f32(0.0416664853692054748535156f));
-    u             = svmad_f32_x(pg, u, s, svdup_f32(0.166666671633720397949219f));
-    u             = svmad_f32_x(pg, u, s, svdup_f32(0.5f));
-
-    u = svadd_f32_x(pg, svdup_f32(1.0f), svmad_f32_x(pg, svmul_f32_x(pg, s, s), u, s));
-
-    // vldexp2(u, q)
-    svint32_t q1     = svasr_n_s32_x(pg, q, 1);
-    svint32_t q2     = svsub_s32_x(pg, q, q1);
-    svint32_t m1     = svlsl_n_s32_x(pg, svadd_s32_x(pg, q1, svdup_s32(127)), 23);
-    svint32_t m2     = svlsl_n_s32_x(pg, svadd_s32_x(pg, q2, svdup_s32(127)), 23);
-    svfloat32_t pow1 = svreinterpret_f32_s32(m1);
-    svfloat32_t pow2 = svreinterpret_f32_s32(m2);
-    u                = svmul_f32_x(pg, svmul_f32_x(pg, u, pow1), pow2);
-
-    // Underflow: x < -104 => 0
-    svbool_t mask_lo   = svcmplt_f32(pg, d, svdup_f32(-104.0f));
-    svint32_t u_as_int = svreinterpret_s32_f32(u);
-    u_as_int           = svsel_s32(mask_lo, svdup_s32(0), u_as_int);
-    u                  = svreinterpret_f32_s32(u_as_int);
-
-    // Overflow: x > 100 => +inf
-    svbool_t mask_hi = svcmplt_f32(pg, svdup_f32(100.0f), d);
-    u                = svsel_f32(mask_hi, svdup_f32(SLEEF_INFINITYf), u);
-
-    return u;
-}
 
 //SVE implementation for RBF kernel post-GEMM part float data type
 template <>
@@ -235,7 +186,7 @@ inline services::Status HelperKernelRBF<float, sve>::postGemmPart(float * const 
         tmp           = svsel_f32(mask, tmp, thresholdVec);
         svst1(pg, &mklBuff[i], tmp);
 
-        svfloat32_t expVal = exp_sleef(tmp);
+        svfloat32_t expVal = daal::internal::ref::RefMath<float, daal::sve>::exp_sleef(tmp);
         svst1(pg, &dataRBlock[i], expVal);
 
         // Block 2
@@ -248,7 +199,7 @@ inline services::Status HelperKernelRBF<float, sve>::postGemmPart(float * const 
         tmp  = svsel_f32(mask, tmp, thresholdVec);
         svst1(pg, &mklBuff[i + step], tmp);
 
-        expVal = exp_sleef(tmp);
+        expVal = daal::internal::ref::RefMath<float, daal::sve>::exp_sleef(tmp);
         svst1(pg, &dataRBlock[i + step], expVal);
 
         // Block 3
@@ -261,7 +212,7 @@ inline services::Status HelperKernelRBF<float, sve>::postGemmPart(float * const 
         tmp  = svsel_f32(mask, tmp, thresholdVec);
         svst1(pg, &mklBuff[i + 2 * step], tmp);
 
-        expVal = exp_sleef(tmp);
+        expVal = daal::internal::ref::RefMath<float, daal::sve>::exp_sleef(tmp);
         svst1(pg, &dataRBlock[i + 2 * step], expVal);
     }
 
@@ -278,7 +229,7 @@ inline services::Status HelperKernelRBF<float, sve>::postGemmPart(float * const 
         tmp           = svsel_f32(mask, tmp, thresholdVec);
         svst1(tail_pg, &mklBuff[i], tmp);
 
-        svfloat32_t expVal = exp_sleef(tmp);
+        svfloat32_t expVal = daal::internal::ref::RefMath<float, daal::sve>::exp_sleef(tmp);
         svst1(tail_pg, &dataRBlock[i], expVal);
     }
 
--- cpp/daal/src/externals/service_math_ref.h
+++ cpp/daal/src/externals/service_math_ref.h
@@ -28,6 +28,14 @@
 #include <cmath>
 #include <limits>
 #include "src/services/service_defines.h"
+#if defined(TARGET_ARM)
+    #if (__CPUID__(DAAL_CPU) == __sve__)
+        #include <arm_sve.h>
+        #define R_LN2f 1.4426950408889634f       // log2(e)
+        #define L2Uf   0.693145751953125f        // high part of ln(2)
+        #define L2Lf   1.428606765330187045e-06f //  low part of ln(2); L2Uf+L2Lf ≈ ln(2)
+    #endif
+#endif
 
 namespace daal
 {
@@ -221,8 +229,72 @@ struct RefMath<float, cpu>
         for (SizeType i = 0; i < n; ++i) out[i] = erf(in[i]);
     }
 
+#if defined(TARGET_ARM)
+    #if (__CPUID__(DAAL_CPU) == __sve__)
+    static svfloat32_t exp_sleef(svfloat32_t d)
+    {
+        svbool_t pg = svptrue_b32();
+
+        // q = (int)(d * R_LN2f)
+        svfloat32_t qf = svmul_f32_x(pg, d, svdup_f32(R_LN2f));
+        svint32_t q    = svcvt_s32_f32_x(pg, svrintn_f32_x(pg, qf));
+
+        // s = d - q * L2Uf - q * L2Lf
+        svfloat32_t s = svmad_f32_x(pg, svcvt_f32_s32_x(pg, q), svdup_f32(-L2Uf), d);
+        s             = svmad_f32_x(pg, svcvt_f32_s32_x(pg, q), svdup_f32(-L2Lf), s);
+
+        // Polynomial evaluation
+        svfloat32_t u = svdup_f32(0.000198527617612853646278381f);
+        u             = svmad_f32_x(pg, u, s, svdup_f32(0.00139304355252534151077271f));
+        u             = svmad_f32_x(pg, u, s, svdup_f32(0.00833336077630519866943359f));
+        u             = svmad_f32_x(pg, u, s, svdup_f32(0.0416664853692054748535156f));
+        u             = svmad_f32_x(pg, u, s, svdup_f32(0.166666671633720397949219f));
+        u             = svmad_f32_x(pg, u, s, svdup_f32(0.5f));
+
+        u = svadd_f32_x(pg, svdup_f32(1.0f), svmad_f32_x(pg, svmul_f32_x(pg, s, s), u, s));
+
+        // vldexp2(u, q)
+        svint32_t q1     = svasr_n_s32_x(pg, q, 1);
+        svint32_t q2     = svsub_s32_x(pg, q, q1);
+        svint32_t m1     = svlsl_n_s32_x(pg, svadd_s32_x(pg, q1, svdup_s32(127)), 23);
+        svint32_t m2     = svlsl_n_s32_x(pg, svadd_s32_x(pg, q2, svdup_s32(127)), 23);
+        svfloat32_t pow1 = svreinterpret_f32_s32(m1);
+        svfloat32_t pow2 = svreinterpret_f32_s32(m2);
+        u                = svmul_f32_x(pg, svmul_f32_x(pg, u, pow1), pow2);
+
+        // Underflow: x < -104 => 0
+        svbool_t mask_lo   = svcmplt_f32(pg, d, svdup_f32(-104.0f));
+        svint32_t u_as_int = svreinterpret_s32_f32(u);
+        u_as_int           = svsel_s32(mask_lo, svdup_s32(0), u_as_int);
+        u                  = svreinterpret_f32_s32(u_as_int);
+
+        // Overflow: x > 100 => +inf
+        svbool_t mask_hi = svcmplt_f32(pg, svdup_f32(100.0f), d);
+        u                = svsel_f32(mask_hi, svdup_f32(__builtin_inff()), u);
+
+        return u;
+    }
+    #endif
+#endif
+
     static void vExp(SizeType n, const float * in, float * out)
     {
+#if defined(TARGET_ARM) && (__CPUID__(DAAL_CPU) == __sve__)
+        // SVE code
+        SizeType i = 0;
+        svbool_t pg;
+
+        for (; i < n; i += svcntw())
+        {
+            pg = svwhilelt_b32(i, n);
+
+            svfloat32_t vin  = svld1(pg, &in[i]);
+            svfloat32_t vout = exp_sleef(vin);
+            svst1(pg, &out[i], vout);
+        }
+        return;
+#endif
+
 #pragma omp simd
         for (SizeType i = 0; i < n; ++i) out[i] = exp(in[i]);
     }
