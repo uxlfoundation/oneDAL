1e4b7d864de9f29e716d09f7be64ca1b384d3a53
Victoriya Fedotova <victoriya.s.fedotova@intel.com>
2025-09-10 09:59:44 -0700

    formatting

--- cpp/oneapi/dal/algo/triangle_counting/backend/cpu/relabel_kernel.hpp
+++ cpp/oneapi/dal/algo/triangle_counting/backend/cpu/relabel_kernel.hpp
@@ -102,7 +102,8 @@ void parallel_prefix_sum(const std::int32_t* degrees_relabel,
             part_prefix[block + 2] += shuffle1[2] + shuffle2[2];
             part_prefix[block + 3] += shuffle1[3] + shuffle2[3];
         }
-        for (std::int64_t block = ((num_blocks + 2) / simd_width - 1) * simd_width + 1; block < num_blocks;
+        for (std::int64_t block = ((num_blocks + 2) / simd_width - 1) * simd_width + 1;
+             block < num_blocks;
              block++) {
             part_prefix[block] = part_prefix[block - 1] + local_sums[block - 1];
         }
