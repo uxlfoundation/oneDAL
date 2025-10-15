4db28c0241ee2bc041c9cf0e32285db8ac0171ab
Antonio De Caro <antonio.decaro99@outlook.it>
2025-10-15 19:29:23 +0200

    frontier: remove unnecessary bitset include from graph header

--- cpp/oneapi/dal/backend/primitives/frontier/graph.hpp
+++ cpp/oneapi/dal/backend/primitives/frontier/graph.hpp
@@ -20,7 +20,6 @@
 #include "oneapi/dal/common.hpp"
 #include "oneapi/dal/graph/detail/common.hpp"
 #include "oneapi/dal/graph/detail/container.hpp"
-#include "oneapi/dal/backend/primitives/frontier/bitset.hpp"
 #include "oneapi/dal/backend/primitives/ndarray.hpp"
 
 namespace oneapi::dal::backend::primitives {
