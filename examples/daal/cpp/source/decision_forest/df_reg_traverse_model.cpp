48cd236cbd76bd89462b6903d36281c00d940df7
Alexandr-Solovev <aleksandr.solovev@intel.com>
2026-01-08 06:24:27 -0800

    fixes

--- examples/daal/cpp/source/decision_forest/df_reg_traverse_model.cpp
+++ examples/daal/cpp/source/decision_forest/df_reg_traverse_model.cpp
@@ -36,15 +36,13 @@ using namespace daal::data_management;
 using namespace daal::algorithms::decision_forest::regression;
 
 /* Input data set parameters */
-const std::string trainDatasetFileName = "data/df_regression_train.csv";
-const size_t categoricalFeaturesIndices[] = { 3 };
-const size_t nFeatures = 13; /* Number of features in training and testing data sets */
+const std::string trainDatasetFileName = "data/df_regression_train_data.csv";
+const std::string trainDatasetLabelFileName = "data/df_regression_train_label.csv";
 
 /* Decision forest parameters */
 const size_t nTrees = 2;
 
 training::ResultPtr trainModel();
-void loadData(const std::string& fileName, NumericTablePtr& pData, NumericTablePtr& pDependentVar);
 void printModel(const daal::algorithms::decision_forest::regression::Model& m);
 
 int main(int argc, char* argv[]) {
@@ -58,17 +56,24 @@ int main(int argc, char* argv[]) {
 
 training::ResultPtr trainModel() {
     /* Create Numeric Tables for training data and dependent variables */
-    NumericTablePtr trainData;
-    NumericTablePtr trainDependentVariable;
-
-    loadData(trainDatasetFileName, trainData, trainDependentVariable);
+    /* Initialize FileDataSource<CSVFeatureManager> to retrieve the input data
+     * from a .csv file */
+    FileDataSource<CSVFeatureManager> trainDataSource(trainDatasetFileName,
+                                                      DataSource::doAllocateNumericTable,
+                                                      DataSource::doDictionaryFromContext);
+    FileDataSource<CSVFeatureManager> trainLabelSource(trainDatasetLabelFileName,
+                                                       DataSource::doAllocateNumericTable,
+                                                       DataSource::doDictionaryFromContext);
+    /* Retrieve the data from the input file */
+    trainDataSource.loadDataBlock();
+    trainLabelSource.loadDataBlock();
 
     /* Create an algorithm object to train the decision forest regression model with the default method */
     training::Batch<> algorithm;
 
     /* Pass a training data set and dependent values to the algorithm */
-    algorithm.input.set(training::data, trainData);
-    algorithm.input.set(training::dependentVariable, trainDependentVariable);
+    algorithm.input.set(training::data, trainDataSource.getNumericTable());
+    algorithm.input.set(training::dependentVariable, trainLabelSource.getNumericTable());
 
     algorithm.parameter().nTrees = nTrees;
 
@@ -77,29 +82,6 @@ training::ResultPtr trainModel() {
 
     /* Retrieve the algorithm results */
     return algorithm.getResult();
-}
-
-void loadData(const std::string& fileName, NumericTablePtr& pData, NumericTablePtr& pDependentVar) {
-    /* Initialize FileDataSource<CSVFeatureManager> to retrieve the input data from a .csv file */
-    FileDataSource<CSVFeatureManager> trainDataSource(fileName,
-                                                      DataSource::notAllocateNumericTable,
-                                                      DataSource::doDictionaryFromContext);
-
-    /* Create Numeric Tables for training data and dependent variables */
-    pData.reset(new HomogenNumericTable<>(nFeatures, 0, NumericTable::notAllocate));
-    pDependentVar.reset(new HomogenNumericTable<>(1, 0, NumericTable::notAllocate));
-    NumericTablePtr mergedData(new MergedNumericTable(pData, pDependentVar));
-
-    /* Retrieve the data from input file */
-    trainDataSource.loadDataBlock(mergedData.get());
-
-    NumericTableDictionaryPtr pDictionary = pData->getDictionarySharedPtr();
-    for (size_t i = 0,
-                n = sizeof(categoricalFeaturesIndices) / sizeof(categoricalFeaturesIndices[0]);
-         i < n;
-         ++i)
-        (*pDictionary)[categoricalFeaturesIndices[i]].featureType =
-            data_feature_utils::DAAL_CATEGORICAL;
 }
 
 /** Visitor class implementing TreeNodeVisitor interface, prints out tree nodes of the model when it is called back by model traversal method */
