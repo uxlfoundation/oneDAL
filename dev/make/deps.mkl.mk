f49463468532b00bc839373f0335ba22e7d9f333
Alexandr-Solovev <aleksandr.solovev@intel.com>
2026-02-27 06:05:12 -0800

    fixes win

--- dev/make/deps.mkl.mk
+++ dev/make/deps.mkl.mk
@@ -82,11 +82,7 @@ mkl_flags.lnx32e := -L$(MKLROOT)/lib \
     -lsycl -lpthread -lm -ldl
 
 
-mkl_flags.win32e := mkl_sycl_blas_dll.lib \
-    mkl_sycl_lapack_dll.lib mkl_sycl_sparse_dll.lib \
-    mkl_sycl_rng_dll.lib \
-    mkl_intel_ilp64_dll.lib \
-    mkl_tbb_thread_dll.lib mkl_core_dll.lib -fsycl
+mkl_flags.win32e :=  -fsycl  mkl_sycl_blas_dll.lib mkl_sycl_lapack_dll.lib mkl_sycl_dft_dll.lib mkl_sycl_sparse_dll.lib mkl_sycl_vm_dll.lib mkl_sycl_rng_dll.lib mkl_sycl_stats_dll.lib mkl_sycl_data_fitting_dll.lib mkl_intel_ilp64_dll.lib mkl_tbb_thread_dll.lib mkl_core_dll.lib OpenCL.lib
 
 mkl_flags.mac32e :=
 mkl_flags.lnxarm :=
