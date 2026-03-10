9a325d8598622aef41f0a32fe51b3bb5f4dab625
Alexandr-Solovev <aleksandr.solovev@intel.com>
2026-03-10 01:47:06 -0700

    fixes for paths

--- samples/oneapi/cpp/ccl/sources/input_helpers.hpp
+++ samples/oneapi/cpp/ccl/sources/input_helpers.hpp
@@ -34,10 +34,17 @@ inline bool check_file(const std::string& name) {
 inline const std::string get_data_path(const std::string& name) {
     const std::vector<std::string> paths = { []() {
                                                 if (const char* root = std::getenv("DALROOT")) {
-                                                    return std::string(root);
+                                                    return std::string(root) + "/share/doc/data";
                                                 }
                                                 return std::string{};
                                             }(),
+                                             []() {
+                                                 if (const char* root = std::getenv("DALROOT")) {
+                                                     return std::string(root) + "/data";
+                                                 }
+                                                 return std::string{};
+                                             }(),
+                                             "../../../data",
                                              "../../data",
                                              "../data" };
 
