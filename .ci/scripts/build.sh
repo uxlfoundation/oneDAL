596711b8aa868e0ae02a73903c1369c6921b0e40
Alexandr-Solovev <aleksandr.solovev@intel.com>
2026-03-18 08:19:56 -0700

    fixes for comments

--- .ci/scripts/build.sh
+++ .ci/scripts/build.sh
@@ -177,7 +177,7 @@ fi
 
 #main actions
 echo "Call env scripts"
-if [ "${backend_config}" == "mkl" ] && [ "${skip_mkl_env}" != "yes" ]; then
+if [ "${backend_config}" == "mkl" ] && [ -z "${MKLROOT}" ]; then
     echo "Sourcing MKL env..."
     source /opt/intel/oneapi/mkl/latest/env/vars.sh
 elif [ "${backend_config}" == "ref" ] && [ ! -z "${BLAS_INSTALL_DIR}" ]; then
