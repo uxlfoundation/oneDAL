573556d2b56adc5f3f796afa7f029bb55b628f1f
Victoriya Fedotova <victoriya.s.fedotova@intel.com>
2026-07-08 02:44:06 -0700

    Fix Windows make build

--- dev/make/compiler_definitions/icx.mkl.32e.mk
+++ dev/make/compiler_definitions/icx.mkl.32e.mk
@@ -74,7 +74,8 @@ endif
 COMPILER.lnx.icx = icx -m64 \
                      -Werror -Wno-empty-body -Wreturn-type -qopenmp-simd ${CXXFLAGS} $(-stdalloc.icx)
 COMPILER.lnx.icx += $(if $(filter yes,$(GCOV_ENABLED)),-coverage,)
-COMPILER.win.icx = icx $(if $(MSVC_RT_is_release),-MD -Qopenmp-simd, -MDd) -nologo -WX -Wno-deprecated-declarations ${CXXFLAGS} $(-stdalloc.icx)
+COMPILER.win.icx = icx $(if $(MSVC_RT_is_release),-MD -Qopenmp-simd, -MDd) -nologo -WX \
+                     -Wno-deprecated-declarations -Wno-empty-body ${CXXFLAGS} $(-stdalloc.icx)
 
 linker.ld.flag := $(if $(LINKER),-fuse-ld=$(LINKER),)
 
