5f4c63ad6777d3b2eff3f790aba0496bab5d59e1
nithin-aashik-mcw <nithin.aashik@multicorewareinc.com>
2026-07-23 02:50:13 -0700

    Add arm64 support for oneTBB download and build in tbb.bat

--- .ci/env/tbb.bat
+++ .ci/env/tbb.bat
@@ -19,10 +19,16 @@ rem req: PowerShell 3.0+
 powershell.exe -command "if ($PSVersionTable.PSVersion.Major -ge 3) {exit 1} else {Write-Host \"The script requires PowerShell 3.0 or above (current version: $($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor))\"}" && goto Error_load
 
 set TBBVERSION=2023.0.0
-set TBBURLROOT=https://github.com/uxlfoundation/oneTBB/releases/download/v%TBBVERSION%/
-set TBBPACKAGE=oneapi-tbb-%TBBVERSION%-win
+if /I "%PROCESSOR_ARCHITECTURE%"=="ARM64" (
+    set TBBURLROOT=https://github.com/uxlfoundation/oneTBB/archive/refs/tags/v%TBBVERSION%
+    set TBBPACKAGE=
+) else (
+    set TBBURLROOT=https://github.com/uxlfoundation/oneTBB/releases/download/v%TBBVERSION%/
+    set TBBPACKAGE=oneapi-tbb-%TBBVERSION%-win
+)
 
 set TBBURL=%TBBURLROOT%%TBBPACKAGE%.zip
+
 if /i "%1"=="" (
     set DST=%~dp0..\..\__deps\tbb
 ) else (
@@ -37,11 +43,35 @@ if not exist "%DST%\win\bin" (
     powershell.exe -command "(New-Object System.Net.WebClient).DownloadFile('%TBBURL%', '%DST%\%TBBPACKAGE%.zip')" && goto Unpack || goto Error_load
 
 :Unpack
-    powershell.exe -command "if (Get-Command Add-Type -errorAction SilentlyContinue) {Add-Type -Assembly \"System.IO.Compression.FileSystem\"; try { [IO.Compression.zipfile]::ExtractToDirectory(\"%DST%\%TBBPACKAGE%.zip\", \"%DST%\") ; Copy-Item \"%DST%\oneapi-tbb-%TBBVERSION%\*\" -Destination \"%DST%\win\tbb\" -Recurse }catch{$_.exception ; exit 1}} else {exit 1}" || goto Error_unpack
-
+    if /I "%PROCESSOR_ARCHITECTURE%"=="ARM64" (
+        powershell.exe -command "if (Get-Command Add-Type -errorAction SilentlyContinue) {Add-Type -Assembly \"System.IO.Compression.FileSystem\"; try { [IO.Compression.zipfile]::ExtractToDirectory(\"%DST%\%TBBPACKAGE%.zip\", \"%DST%\") ; }catch{$_.exception ; exit 1}} else {exit 1}" && goto Build_oneTBB || goto Error_unpack
+    ) else (
+        powershell.exe -command "if (Get-Command Add-Type -errorAction SilentlyContinue) {Add-Type -Assembly \"System.IO.Compression.FileSystem\"; try { [IO.Compression.zipfile]::ExtractToDirectory(\"%DST%\%TBBPACKAGE%.zip\", \"%DST%\") ; Copy-Item \"%DST%\oneapi-tbb-%TBBVERSION%\*\" -Destination \"%DST%\win\tbb\" -Recurse }catch{$_.exception ; exit 1}} else {exit 1}" || goto Error_unpack
+    )
     if not exist %DST%\win\tbb\redist\intel64\vc14 powershell.exe -command "New-Item -Path \"%DST%\win\tbb\redist\intel64\vc14\" -ItemType Directory"
-
     goto Exit 
+
+:Build_oneTBB
+    IF "%VS_VER%"=="2026_build_tools" (
+        @call "C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" %PROCESSOR_ARCHITECTURE%
+    ) ELSE IF "%VS_VER%"=="2019_build_tools" (
+        @call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" %PROCESSOR_ARCHITECTURE%
+    ) ELSE IF "%VS_VER%"=="2017_build_tools" (
+        @call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" %PROCESSOR_ARCHITECTURE%
+    )
+    pushd "%DST%\oneTBB-%TBBVERSION%"
+        rmdir /s /q build-arm64
+        cmake -B build-arm64 -S . -GNinja -DTBB_TEST=OFF -DCMAKE_INSTALL_PREFIX="%DST%\win\tbb"
+        cmake --build build-arm64
+        cmake --install build-arm64
+        mkdir "%DST%\win\tbb\redist\%PROCESSOR_ARCHITECTURE%\vc14" 2>nul
+        mkdir "%DST%\win\tbb\lib\%PROCESSOR_ARCHITECTURE%\vc14" 2>nul
+
+        robocopy "%DST%\win\tbb\bin" "%DST%\win\tbb\redist\%PROCESSOR_ARCHITECTURE%\vc14" /E /MOV
+        robocopy "%DST%\win\tbb\lib" "%DST%\win\tbb\lib\%PROCESSOR_ARCHITECTURE%\vc14" *.lib /MOV
+        rmdir /s /q "%DST%\win\tbb\bin"
+    popd
+    exit /B 0
 
 :Error_load
     echo tbb.bat : Error: Failed to load %TBBURL% to %DST%, try to load it manually
