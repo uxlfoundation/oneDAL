4c5d90529a3df7d2d18d1101e36e9f592c8eb38b
Ian Faust <icfaust@gmail.com>
2026-08-03 00:47:44 +0200

    Update communicator.hpp

--- cpp/oneapi/dal/spmd/communicator.hpp
+++ cpp/oneapi/dal/spmd/communicator.hpp
@@ -545,6 +545,13 @@ communicator<device_memory_access::none> make_communicator() {
     throw communication_error(dal::detail::error_messages::unsupported_communicator_backend());
 }
 
+template <typename Backend>
+communicator<device_memory_access::none> make_communicator(std::int64_t comm_handle) {
+    static_assert(!std::is_same_v<Backend, Backend>, "Unsupported communicator backend");
+
+    throw communication_error(dal::detail::error_messages::unsupported_communicator_backend());
+}
+
 #ifdef ONEDAL_DATA_PARALLEL
 template <typename Backend>
 communicator<device_memory_access::usm> make_communicator(sycl::queue& queue) {
@@ -552,6 +559,14 @@ communicator<device_memory_access::usm> make_communicator(sycl::queue& queue) {
 
     throw communication_error(dal::detail::error_messages::unsupported_communicator_backend());
 }
+
+template <typename Backend>
+communicator<device_memory_access::usm> make_communicator(sycl::queue& queue,
+                                                            std::int64_t comm_handle) {
+    static_assert(!std::is_same_v<Backend, Backend>, "Unsupported communicator backend");
+
+    throw communication_error(dal::detail::error_messages::unsupported_communicator_backend());
+  }
 #endif
 
 } // namespace oneapi::dal::preview::spmd
