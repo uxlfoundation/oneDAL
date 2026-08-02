45a0d4c1af7681b86bae3e45023a11e84254a170
Ian Faust <icfaust@gmail.com>
2026-08-03 00:28:25 +0200

    Update communicator.hpp

--- cpp/oneapi/dal/detail/mpi/communicator.hpp
+++ cpp/oneapi/dal/detail/mpi/communicator.hpp
@@ -120,9 +120,9 @@ public:
             : mpi_comm_(MPI_COMM_WORLD),
               default_root_(default_root) {}
 
-  explicit mpi_communicator_impl(MPI_Comm comm, std::int64_t default_root = 0)
-          : mpi_comm_(comm),
-            default_root_(default_root) {}
+    explicit mpi_communicator_impl(MPI_Comm comm, std::int64_t default_root = 0)
+            : mpi_comm_(comm),
+              default_root_(default_root) {}
 
 #ifdef ONEDAL_DATA_PARALLEL
     //    template<typename T = MemoryAccessKind, spmd::enable_if_device_memory_accessible_t<T>>
